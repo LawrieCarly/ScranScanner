a58b364ef4b2e25504cbfe29e17f73c138650e94
Ethan <ethanbaird94@gmail.com>
2022-09-27 10:38:15 +0100

    adds Customer

--- service/src/main/java/com/scranscanner/service/models/Customer.java
+++ service/src/main/java/com/scranscanner/service/models/Customer.java
@@ -1,17 +1,57 @@
 package com.scranscanner.service.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.scranscanner.service.types.PermissionType;
 
 import javax.persistence.*;
+import java.util.HashMap;
 
 @Entity
-@Table(name = "users")
+@Table(name = "customers")
 public class Customer extends User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private Long id;
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnoreProperties({"customer"})
+    private List<Booking> bookings;
+
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnoreProperties({"bookings", "tables", "reviews"})
+    private List<Restaurant> savedRestaurants;
 
     @Column
+    private HashMap<String, String> preferences;
+
+    public Customer() {
+    }
+
+    public Customer(String displayName, String email, String password, PermissionType permissionType, List<Booking> bookings, List<Restaurant> savedRestaurants, HashMap<String, String> preferences) {
+        super(displayName, email, password, permissionType);
+        this.bookings = bookings;
+        this.savedRestaurants = savedRestaurants;
+        this.preferences = preferences;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public List<Restaurant> getSavedRestaurants() {
+        return savedRestaurants;
+    }
+
+    public void setSavedRestaurants(List<Restaurant> savedRestaurants) {
+        this.savedRestaurants = savedRestaurants;
+    }
+
+    public HashMap<String, String> getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(HashMap<String, String> preferences) {
+        this.preferences = preferences;
+    }
 }
