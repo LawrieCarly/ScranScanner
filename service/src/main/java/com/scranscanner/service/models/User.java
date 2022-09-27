0e1aaad0a12ad45e8b4d9d21017c147d48f9459f
Ethan <ethanbaird94@gmail.com>
2022-09-27 10:15:19 +0100

    adds User Class

---
+++ service/src/main/java/com/scranscanner/service/models/User.java
@@ -0,0 +1,51 @@
+package com.scranscanner.service.models;
+
+public abstract class User {
+
+    private String displayName;
+
+    private String email;
+
+    private String password;
+
+    private PermissionType permissionType;
+
+    public User(String displayName, String email, String password, PermissionType permissionType) {
+        this.displayName = displayName;
+        this.email = email;
+        this.password = password;
+        this.permissionType = permissionType;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PermissionType getPermissionType() {
+        return permissionType;
+    }
+
+    public void setPermissionType(PermissionType permissionType) {
+        this.permissionType = permissionType;
+    }
+}
