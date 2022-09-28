edd3ea087399770b773b314e76d571f50da436fb
Ethan <ethanbaird94@gmail.com>
2022-09-28 16:15:56 +0100

    adds Availability

--- server/src/main/java/com/scranscanner/service/models/DinnerTable.java
+++ server/src/main/java/com/scranscanner/service/models/DinnerTable.java
@@ -5,9 +5,7 @@ import com.scranscanner.service.types.PriorityType;
 
 import javax.persistence.*;
 import javax.persistence.Table;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Entity
@@ -38,8 +36,11 @@ public class DinnerTable {
 //    @Column
 //    private List<DinnerTable> joinables;
 
-    @Column
-    private HashMap<LocalTime, Boolean> timeSlots;
+//    @Column
+//    private HashMap<LocalTime, Boolean> timeSlots;
+
+    @OneToMany(mappedBy = "dinnerTable")
+    private List<Availability> availabilities;
 
     @OneToMany(mappedBy = "dinnerTable")
     private List<Booking> bookings;
@@ -52,7 +53,7 @@ public class DinnerTable {
         this.size = size;
         this.priorityType = priorityType;
 //        this.joinables = new ArrayList<>();
-        this.timeSlots = new HashMap<>();
+        this.availabilities = new ArrayList<>();
         this.restaurant = restaurant;
         this.bookings = new ArrayList<>();
     }
@@ -113,12 +114,12 @@ public class DinnerTable {
 //        this.joinables = joinables;
 //    }
 
-    public HashMap<LocalTime, Boolean> getTimeSlots() {
-        return timeSlots;
+    public List<Availability> getAvailabilities() {
+        return availabilities;
     }
 
-    public void setTimeSlots(HashMap<LocalTime, Boolean> timeSlots) {
-        this.timeSlots = timeSlots;
+    public void setAvailabilities(List<Availability> availabilities) {
+        this.availabilities = availabilities;
     }
 }
 
