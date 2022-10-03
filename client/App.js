94c3e09864376dcb8213a5a0ce0c1ddc585b6b8c
Ethan <ethanbaird94@gmail.com>
2022-10-03 11:34:43 +0100

    adds login states to app.js

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,6 +13,11 @@ import TabProfileScreen from './screens/TabProfileScreen';
 import ReservationsScreen from './screens/ReservationsScreen';
 import FavouritesScreen from './screens/FavouritesScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import LoginContainer from './containers/LoginContainer';
+
+// Login states
+
+
 
 
 // Navigator 'stack' function to allow page navigations from within the Profile Tab
@@ -43,20 +49,39 @@ function MyTabsScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [customerId, setCustomerId] = useState();
+
+  const flipLoggedIn = () => {
+    setLoggedIn(!loggedIn);
+  }
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={TabHomeScreen} />
-        <Tab.Screen name="Search" component={TabSearchResultsScreen} />
-        <Tab.Screen name="Notifications" component={MyTabsScreen} />
-        <Tab.Screen name="Profile" component={TabProfileScreen} />
+    <>
+      {!loggedIn? 
+      <>
+        <LoginContainer/>
+      </>
+           : 
+      <>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Home" component={TabHomeScreen} />
+            <Tab.Screen name="Search" component={TabSearchResultsScreen} />
+            <Tab.Screen name="Notifications" component={MyTabsScreen} />
+            <Tab.Screen name="Profile" component={TabProfileScreen} />
 
 
-        <Tab.Screen name="Restaurant" component={RestaurantScreen} 
-            options={{tabBarButton: ()=> null,tabBarVisible: false,}}   
-               />
-      </Tab.Navigator>
-    </NavigationContainer>
+            <Tab.Screen name="Restaurant" component={RestaurantScreen} 
+              options={{tabBarButton: ()=> null,tabBarVisible: false,}}   
+            />
+        </Tab.Navigator>
+      </NavigationContainer>
+      </>}
+    </>
+      
+  
   );
 }
 
---
+++ client/containers/LoginContainer.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+const LoginContainer = () => {
+    return(
+        <Text>This is the login container</Text>
+    )
+}
+
+export default LoginContainer
