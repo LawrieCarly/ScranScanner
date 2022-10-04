082f1d2e2c5ef5bf3e4e7c51b226d60d87fc9095
Samuel <samuelsf@hotmail.co.uk>
2022-10-04 20:13:47 +0100

    added catches to service files

--- client/services/SearchService.js
+++ client/services/SearchService.js
@@ -5,9 +5,12 @@ const baseURL = 'http://192.168.0.20:8080/restaurants/';
 
 
 export const getRestaurants = () => {
+  try {
   return fetch(baseURL)
     .then(res => res.json());
-    
+  } catch (error) {
+    console.error(error);
+    }
 }
 
 // export async function getRestaurantById(id) {
@@ -17,6 +20,7 @@ export const getRestaurants = () => {
 // }
 
 export const getSearchResults = (partySize, date, time) => {
+  try {
   const formattedDate = moment(date).format('YYYY-MM-DD')
   const formattedTime = moment(date).format('HH:mm')
   // console.log(formattedDate)
@@ -24,7 +28,9 @@ export const getSearchResults = (partySize, date, time) => {
 
   return fetch(baseURL + 'filtered/?partySize=' + partySize +'&date=' + formattedDate + '&time=' + formattedTime)
   .then(data => data.json());
-    
+  } catch (error) {
+    console.error(error);
+    }   
 }
 
 
