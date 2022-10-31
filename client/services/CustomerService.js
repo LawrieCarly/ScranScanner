9a31353073f0e8a22275da02a9d0535ea158e0a0
Ethan <ethanbaird94@gmail.com>
2022-10-31 15:43:38 +0000

    tidies CustomerService.js

--- client/services/CustomerService.js
+++ client/services/CustomerService.js
@@ -4,33 +4,31 @@ const baseURL = `http://${REACT_APP_DEV_IP}:8080/customers/`;
 
 export async function getCustomerById(id) {
     try {
-    const data = await fetch(baseURL + id);
-    return data.json();
-    } catch (error) {
+        const data = await fetch(baseURL + id);
+        return data.json();
+    } 
+    catch (error) {
         console.error(error);
-        }
+    }
 }
 
-
 export const getCustomerByEmail = (email) => {
-    console.log(baseURL);
     try {
-    console.log(email);
-    return fetch(baseURL + 'byEmail/' + email)
-        .then(res => res.json());
-    } catch (error) {
+        const data = fetch(baseURL + 'byEmail/' + email);
+        return data.json();
+    } 
+    catch (error) {
         console.error(error);
-        }
+    }
 
 }
 
 export const addRestaurantToCustomerFavourites = (customerId, restaurantId) => {
     try {
-    return fetch(baseURL + customerId + '/restaurant/' + restaurantId,{
-        method: "PATCH"
-    })
-        .then(res => res.json());
-    } catch (error) {
+        const data = fetch(baseURL + customerId + '/restaurant/' + restaurantId, {method: "PATCH"});
+        return data.json();
+    } 
+    catch (error) {
         console.error(error);
-        }
+    }
 }
